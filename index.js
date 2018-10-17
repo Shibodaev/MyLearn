a41bd1b3ea58d37bfe30d9c62f4d3b353f4baab9
Shibodaev <shibodaev@intervolga.ru>
2018-10-17 22:01:08 +0300

    v0.0.3

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 $(function () {
-
+	$(".w-button").click(function (e) {
+		let numId = Number(e.target.id);
+		if (numId == 1) {
+			$.get("ajax.html", success);
+		}
+		if (numId == 2) {
+			$.get("ajax2.html", success)
+		}
+		if (numId == 3) {
+			$.get("ajax3.html", success)
+		}
+		if (numId == 4) {
+			$.get("ajax4.html", success)
+		}
+	})
+	function success(data) {
+		$('#content').html(data);
+	}
 });
 
 
