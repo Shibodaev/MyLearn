2a8411d3d822ea25ab5ad3c53d56044b6995b430
Shibodaev <shibodaev@intervolga.ru>
2018-10-17 23:01:24 +0300

    v 0.0.4 Из учебника

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
 $(function () {
-	$(".w-button").click(function (e) {
-		let numId = Number(e.target.id);
-		if (numId == 1) {
-			$.get("ajax.html", success);
-		}
-		if (numId == 2) {
-			$.get("ajax2.html", success)
-		}
-		if (numId == 3) {
-			$.get("ajax3.html", success)
-		}
-		if (numId == 4) {
-			$.get("ajax4.html", success)
-		}
+	$("button").click(function () {
+		$.get("ajax" + $(this).attr("data-ajax") + ".html", success);
 	})
 	function success(data) {
-		$('#content').html(data);
+		$("#content").html(data);
 	}
-});
-
+})
 
