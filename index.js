b238b8230a0fb45df27f7fc31cb355036849c11f
Shibodaev <shibodaev@intervolga.ru>
2018-10-21 00:18:41 +0300

    Мое решение

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 // Свойства объекта Sum
 // У нас есть объект хранения зарплат нашей команды:
-let salaries = {
+var salaries = {
 	John: 100,
 	Ann: 160,
 	Pete: 130
 }
+function summation(obj) {
+	let sum = 0;
+	if (obj != undefined) {
+		for (var key in obj) {
+			sum += obj[key];
+		}
+		console.log(sum)
+	} else {
+		return 0;
+	}
+}
+summation(salaries);
+
 // Напишите код, чтобы суммировать все зарплаты и хранить в
 // переменной sum. Должно быть 390в приведенном выше примере.
