52da470bb245f328b962a8c6c176a4db9116f3db
Shibodaev <shibodaev@intervolga.ru>
2018-10-22 22:37:08 +0300

    Моё решение

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 "use strict";
 
-
-
+var salaries = {
+	"Вася": 100,
+	"Петя": 300,
+	"Даша": 250
+};
+function sum(obj) {
+	let summa = 0;
+	for (var key in obj) {
+		summa += obj[key]
+	}
+	console.log(summa);
+}
+sum(salaries);
