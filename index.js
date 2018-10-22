310298d1727025d1de2e83866e77c51f37a6de6b
Shibodaev <shibodaev@intervolga.ru>
2018-10-22 22:31:49 +0300

    Вывести сумму всех зарплат

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
 "use strict";
 
-var salaries = {
-	"Вася": 100,
-	"Петя": 300,
-	"Даша": 250
-};
-
-function salary(obj) {
-	var num = 0, name = "";
-	for (var key in obj) {
-		if (obj[key] > num ) {
-			num = obj[key];
-			name = key;
-		}
-	}
-	console.log(name);
-}
-salary(salaries)
 
 
