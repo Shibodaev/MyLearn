cf35627c073406724d764ae66a99540a60363b05
Shibodaev <shibodaev@intervolga.ru>
2018-10-22 22:26:50 +0300

    Моё решение

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-let menu = {
-	width: 200,
-	height: 300,
-	title: "My menu"
+"use strict";
+
+var salaries = {
+	"Вася": 100,
+	"Петя": 300,
+	"Даша": 250
 };
 
-function multiplyNumeric(obj) {
-	for(var key in obj ) {
-		if(typeof obj[key] == "number") {
-			obj[key] *= 2
+function salary(obj) {
+	var num = 0, name = "";
+	for (var key in obj) {
+		if (obj[key] > num ) {
+			num = obj[key];
+			name = key;
 		}
 	}
+	console.log(name);
 }
+salary(salaries)
 
-multiplyNumeric(menu);
-alert( "menu width=" + menu.width + " height=" + menu.height + " title=" + menu.title );
-// after the call
-// menu = {
-// 	width: 400,
-// 	height: 600,
-// 	title: "My menu"
-// };
+
