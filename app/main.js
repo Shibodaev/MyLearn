1bba2c310b5b24ecdbf1096698ad8cc6e4505de9
Shibodaev <shibodaev@mail.ru>
2019-05-09 03:08:17 +0400

    array-task-1 version(2) light

--- app/main.js
+++ app/main.js
@@ -7,10 +7,12 @@
 */
 var goods = [2,1,3,5,4,9,8,7,6];
 
-for( var i = 0; i <= goods.length; i++ ) {
-	if( i == goods.length - 1) {
-		alert(goods[i]);
-	}
-}
+// for( var i = 0; i <= goods.length; i++ ) {
+// 	if( i == goods.length - 1) {
+// 		alert(goods[i]);
+// 	}
+// }
+var lastElement = goods[goods.length - 1];
+alert(lastElement);
 
 
