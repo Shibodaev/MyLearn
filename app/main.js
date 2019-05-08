8ad812b749b284df21df571dc43e8f0ed1b124c7
Shibodaev <shibodaev@mail.ru>
2019-05-09 02:22:58 +0400

    objeckt-task-3

--- app/main.js
+++ app/main.js
@@ -1,19 +1,39 @@
 "use strict";
-var salaries = {
-	"Вася": 100,
-	"Петя": 300,
-	"Даша": 250
-  };
-  var obj = 0, name = "";
-  
+/*
+Создайте функцию multiplyNumeric, которая получает
+объект и умножает все численные свойства на 2. Например:
+P.S. Для проверки на число используйте функцию:
+function isNumeric(n) {
+return !isNaN(parseFloat(n)) && isFinite(n)
+}
+*/
 
-  for(var key in salaries){
-	if(obj < salaries[key] ) {
-		obj = salaries[key];
-		name = key
-	} else if (obj > salaries[key]) {
-		alert(name);
+var menu = {
+	width: 200,
+	height: 300,
+	title: "My menu"
+};
+function multiplyNumeric(object) {
+	for (const key in object) {
+		if (object.hasOwnProperty(key)) {
+			if (isNumeric(object[key])) {
+				object[key] *= 2;
+			} else {
+				return object[key];
+			}
+		}
 	}
-  }
-  
+	function isNumeric(n) {
+		return !isNaN(parseFloat(n)) && isFinite(n);
+	}
+};
+multiplyNumeric(menu);
+console.log(menu);
+
+/*после вызова
+menu = {
+	width: 400,
+	height: 600,
+	title: "My menu"
+};*/
 
