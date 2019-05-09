9e8fb73411b4c76194f56ebd90f7dbe06b112fff
Shibodaev <shibodaev@mail.ru>
2019-05-10 00:29:38 +0400

    МоЁ  решение задачи Поиск в массиве https://learn.javascript.ru/array

--- app/main.js
+++ app/main.js
@@ -1,29 +1,21 @@
 "use strict";
-/*
-Создайте калькулятор для введённых значений
-важность: 4 
-Напишите код, который:
 
-1) Запрашивает по очереди значения при помощи prompt и сохраняет их в массиве.
-2) Заканчивает ввод, как только посетитель введёт пустую строку, не число или нажмёт «Отмена».
-3) При этом ноль 0 не должен заканчивать ввод, это разрешённое число.
-4) Выводит сумму всех значений массива
+/*
+Создайте функцию find(arr, value),
+которая ищет в массиве arr значение value и возвращает его номер,
+если найдено, или -1, если не найдено.
 */
-;(function calculator(prop) {
-    var arr = [];
-    var empty = 0;
-    var number = prompt('Введите число', 0);
-    if (!isNaN(parseFloat(number)) && isFinite(number)) {
-        arr.push(number);
-        calculator();
-    } else if (typeof number == "string" || number == null) {
-        for(var i = 0; i < arr.length; i++){
-           empty += +arr[i];
-        }
-        alert(empty);
-        return false;
-    } else {
-        return false;
+
+function find(arr, value) {
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] === value) {
+            return i;
+        }   
     }
-    arr.push(number);
-})();
+    return -1;
+};
+var arr = ["test", 2, 1.5, false];
+console.log(find(arr, "test")); // 0
+console.log(find(arr, 2)); // 1
+console.log(find(arr, 1.5)); // 2
+console.log(find(arr, 0)); // -1 
