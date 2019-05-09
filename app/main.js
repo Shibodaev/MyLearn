e8e32e7565ee75689fdb90e77d87344db3283478
Shibodaev <shibodaev@mail.ru>
2019-05-09 22:59:56 +0400

    моё решение задачи по массивам learn js Создайте калькулятор для введённых значений

--- app/main.js
+++ app/main.js
@@ -1,18 +1,29 @@
 "use strict";
 /*
-Получить случайное значение из массива
-важность: 3
-Напишите код для вывода alert случайного значения из массива:
+Создайте калькулятор для введённых значений
+важность: 4 
+Напишите код, который:
 
-var arr = ["Яблоко", "Апельсин", "Груша", "Лимон"];
-P.S. Код для генерации случайного целого от min to max включительно:
-
-var rand = min + Math.floor(Math.random() * (max + 1 - min));
+1) Запрашивает по очереди значения при помощи prompt и сохраняет их в массиве.
+2) Заканчивает ввод, как только посетитель введёт пустую строку, не число или нажмёт «Отмена».
+3) При этом ноль 0 не должен заканчивать ввод, это разрешённое число.
+4) Выводит сумму всех значений массива
 */
-
-var arr = ["Яблоко", "Апельсин", "Груша", "Лимон"], min = 0, max = arr.length;
-function randomProperty(prop) {
-	var rand = min + Math.floor(Math.random() * (max + 1 - min))
-	alert(arr[rand]);
-}
-randomProperty();
+;(function calculator(prop) {
+    var arr = [];
+    var empty = 0;
+    var number = prompt('Введите число', 0);
+    if (!isNaN(parseFloat(number)) && isFinite(number)) {
+        arr.push(number);
+        calculator();
+    } else if (typeof number == "string" || number == null) {
+        for(var i = 0; i < arr.length; i++){
+           empty += +arr[i];
+        }
+        alert(empty);
+        return false;
+    } else {
+        return false;
+    }
+    arr.push(number);
+})();
