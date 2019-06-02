d8a67359a994e6e36cd9fbe35bc2651de37e4d59
Shibodaev <shibodaev@mail.ru>
2019-06-02 17:04:29 +0400

    Добавил Тест функцию assert из учебника

--- app/main.js
+++ app/main.js
@@ -1,21 +1,26 @@
 
-/* Напиши получение значений через Prompt и запись их в обьект */          
-getPerson = (name,subname,age) => {
-    // console.log(person);
-    let person = {
-        name: name,
-        subname: subname,
-        age: age,
-        sayHello () {
-            console.log(`Привет меня зовут ${this.name} ${this.subname} мне ${this.age} лет!!!! `);
-        }
-    }
-    return person;
+var root = document.getElementById('root');
+// Функция ТЕСТА
+function whater(a,b,c) {
+    assert(a===1, "Первое зачение" );
+    assert(b===2, "Второе зачение" );
+    assert(c===3, "Третье зачение" );
+    assert(arguments.length === 5, " пять параметров");
+    assert(arguments[0] === a, "верно!!!");
+    assert(arguments[1] === b, "верно!!!");
+    assert(arguments[2] === c, "верно!!!");
+    assert(arguments[3] === 4, console.log(arguments[3]));
+    assert(arguments[4] === 5, console.log(arguments[4]));
 }
+whater(1,2,3,4,5);
 
-const myPerson = getPerson("Mike","Shibodaev",35);
-myPerson.sayHello();
-
-const newPerson = getPerson("Виктория","Ромадина",38);
-newPerson.sayHello();
-
+function assert(value, desc) {
+    var li = document.createElement("li");
+    li.className = value ? "pass" : "fail";
+    li.appendChild(document.createTextNode(desc));
+    document.getElementById("results").appendChild(li);
+}
+window.onload = function() {
+    assert(true, " Thetestsuiteis running.");
+    assert(false, " Fail1 ");
+};
